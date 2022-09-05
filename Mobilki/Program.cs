23c103a2c74979c9e68473078545a6fd7397e4bd
unknown <@ngknn.local>
2022-09-05 18:12:52 +0300

    First Commit

---
+++ Mobilki/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobilki
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Задание 1
+            //for (; ; )
+            //{
+            //    Console.WriteLine("Введите первое вещественное число");
+            //    double x = Convert.ToDouble(Console.ReadLine());
+            //    Console.WriteLine("Введите второе вещественное число");
+            //    double y = Convert.ToDouble(Console.ReadLine());
+            //    if (x < 0 || y < 0)
+            //    {
+            //        Console.WriteLine("Одно из чисел отрицательное");
+            //    }
+            //    else
+            //    {
+            //        double z = Math.Log(x, y);
+            //        Console.WriteLine("Ответ:" + z);
+            //    }
+            //}
+            #endregion
+
+            #region Задание 3
+            //Console.WriteLine("Введите число:");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //int ost1 = x % 3;
+            //int ost2 = x % 4;
+            //if(ost1 == ost2)
+            //{
+            //    Console.WriteLine("Одинаковый остаток");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Разный остаток");
+            //}
+            #endregion
+
+            #region Задание 5
+            //Console.WriteLine("Введите число");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //double otv = Math.Log(Math.Abs(Math.Cos(x)))/ Math.Log(1+ Math.Pow(x,2));
+            //Console.WriteLine(otv);
+            #endregion
+
+            #region Задание 8
+            //Console.WriteLine("Введите 1 число");
+            //double x = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Введите 2 число");
+            //double y = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Введите 3 число");
+            //double z = Convert.ToDouble(Console.ReadLine());
+            //double a = x + y;
+            //Console.WriteLine("1 + 2 числа =" + a);
+            //double b = x + z;
+            //Console.WriteLine("1 + 3 числа =" + b);
+            //double c = z + y;
+            //Console.WriteLine("3 + 2 числа =" + c);
+            //if ( a > 0 && b > 0 && c > 0)
+            //{
+            //    Console.WriteLine("Сумма любых двух чисел не является отрицательной");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Сумма каких-либо 2 чисел отрицательна");
+            //}
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
